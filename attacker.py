712fe3a61c715de23ec05be4e9bd19a068aaaa49
Shatakshi Shukla <140908869+Shatakshi-Shukla-Github@users.noreply.github.com>
2026-02-05 22:57:19 +0530

    Update target IP and packet count for attack simulation

--- attacker.py
+++ attacker.py
@@ -2,9 +2,9 @@ from scapy.all import IP, TCP, send
 import time
 
 # --- Configuration ---
-TARGET_IP = "127.0.0.1"  # Your own machine
+TARGET_IP = "192.168.0.1"  # This is the Gateway. It WILL show up in the GUI.
 TARGET_PORT = 80         # A common port
-PACKET_COUNT = 200       # How many packets to send
+PACKET_COUNT = 2000       # How many packets to send
 
 print(f"Starting simulated attack on {TARGET_IP}...")
 
